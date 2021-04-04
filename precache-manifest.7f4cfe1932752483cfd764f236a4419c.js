76cd23287bfbf5927dfb627548b18cc3925960ab
Gonca <gonca.negis@gmail.com>
2021-04-04 09:20:10 +0300

    Add files via upload

---
+++ precache-manifest.7f4cfe1932752483cfd764f236a4419c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7edd30d46a91c5d0c9e7f28c85731d76",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0a4f46d1f0e513edc228",
+    "url": "/static/css/main.071378ba.chunk.css"
+  },
+  {
+    "revision": "ab85a117f39d501e282e",
+    "url": "/static/js/2.40b1d22a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.40b1d22a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a4f46d1f0e513edc228",
+    "url": "/static/js/main.f5810fd1.chunk.js"
+  },
+  {
+    "revision": "7402b3a51c6a0325ccf5",
+    "url": "/static/js/runtime-main.297f6f71.js"
+  }
+]);
